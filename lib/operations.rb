713d8158689185575b89a1033e58f28afc01397b
Saida Davis <saihdavis@gmail.com>
2020-03-28 20:47:19 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,7 +9,13 @@ def unsafe?(speed)
 end
 
 def not_safe?(speed)
-	 speed > 60 ? "true"
+	 if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  else speed > 40 && speed < 60
+    return false  
+  end
 	 
 end
 	end
